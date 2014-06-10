53705c48807d4bd33bea2ac7d44e3e23681dc2c2
Jonathan Dickerson <jonathandickerson@Jonathans-MacBook-Pro-4.local>
2014-06-10 19:47:08 -0400

    steps 1-9 in homework

--- script.js
+++ script.js
@@ -1,3 +1,105 @@
-var name = 'Jonathan';
-var whoelse = name.concat(' and friends');
+// 5 variables
 
+// var homework = (output) {
+// 	function runMe() {
+var name = 'Jonathan';
+var dog = 'pitbull';
+var car = 'chevy';
+var favnumber = 8;
+var favmultiple = 3;
+
+console.log(name);
+console.log(dog);
+console.log(car);
+console.log(favnumber);
+console.log(favmultiple);
+
+
+// concat
+var whoelse = name.concat(' and friends');
+console.log(whoelse)
+
+// //split 
+name.split('a')
+// //["Jon", "th", "n"]
+
+//join
+
+//petname.length
+// var petname = "dog"
+//  petname.legth
+
+// join
+var animals = ['zebra', 'lion', 'giraffe', 'ox', 'mule', 'donkey', 'ape', 'monkey', 'cat', 'mouse'];
+animals.join()
+
+console.log(animals.push())
+
+animals.pop()
+
+//everse: animals.reverse() ["cat", "monkey", "ape", "donkey", "mule", "ox", "giraffe", "lion", "zebra"]
+
+//splice: animals.splice()["donkey", "mule", "ox", "giraffe", "lion", "zebra"]
+
+var homework = function(
+    var name = 'Jonathan';
+    var dog = 'pitbull';
+    var car = 'chevy';
+    var favnumber = 8;
+    var favmultiple = 3;
+
+    console.log(name); console.log(dog); console.log(car); console.log(favnumber); console.log(favmultiple);
+
+    var whoelse = name.concat(' and friends'); console.log(whoelse); console.log(name.split('a'));
+
+    var petname = "dog"; console.log(petname); console.log(petname.legth);
+
+    var animals = ['zebra', 'lion', 'giraffe', 'ox', 'mule', 'donkey', 'ape', 'monkey', 'cat', 'mouse']; animals.join() console.log(animals); console.log(animals.push()) console.log(animals.pop()); console.log(animals.reverse()); console.log(splice: animals.splice());
+) {
+    return name.split('a');
+}
+
+name.split('a')
+
+//}
+
+// function runMe() {
+
+// var name = 'Jonathan';
+// var dog = 'pitbull';
+// var car = 'chevy';
+// var favnumber = 8;
+// var favmultiple = 3;
+
+// // concat
+// var whoelse = name.concat(' and friends');
+// //console.log(whoelse)
+
+// //split 
+// name.split('a')
+// //["Jon", "th", "n"]
+
+// //join
+
+// //petname.length
+// var petname = "dog"
+// // petname.legth
+
+// // join
+// var animals = ['zebra', 'lion', 'giraffe', 'ox', 'mule', 'donkey', 'ape', 'monkey', 'cat', 'mouse'];
+// //animals.join()
+
+// //push: animals.push(turtle) 11
+
+// //pop: animals.pop() "turtle"
+
+// //reverse: animals.reverse() ["cat", "monkey", "ape", "donkey", "mule", "ox", "giraffe", "lion", "zebra"]
+
+// //splice: animals.splice()["donkey", "mule", "ox", "giraffe", "lion", "zebra"]
+
+// //function homework (output) {
+// 	//console.log('name.split('a')', 'petname.legth');
+// }
+
+
+// }
