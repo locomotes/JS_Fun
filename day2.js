6c90b5b5f3d4eb6c2ddcb87c9a94afded8e8d30b
Jonathan Dickerson <jonathandickerson@Jonathans-MacBook-Pro-4.local>
2014-06-11 22:15:35 -0400

    homework up until objects

---
+++ day2.js
@@ -0,0 +1,76 @@
+// Homework June 11th
+
+// addition 
+var testing = function() {
+    var building1 = 5000;
+    var building2 = 2500;
+    var building3 = 3500;
+    console.log(building1 + building2 + building3);
+};
+testing();
+
+// subtraction
+var subtract = function() {
+    var num1 = 73;
+    var num2 = 13;
+    var num3 = 4;
+    console.log(num1 - num3 - num2);
+};
+subtract();
+
+// multiplication
+var mult = function() {
+    return (43 * 12);
+};
+mult();
+
+// division
+var div = function() {
+    return (200 / 12);
+}
+div();
+
+// Preform some simple conditional statements using "if" and "else" and make sure to use variables
+var par = function() {
+    var x = 43;
+    if (x > 8) {
+        return ("xwins");
+    } else {
+        return ("xlooses");
+    };
+};
+par();
+
+var sixpackofbeer = function(y) {
+    if (y === 6) {
+        return ("drink");
+    } else {
+        return ("ask for a discount");
+    };
+};
+sixpackofbeer();
+
+// Create 10 different objects whose properties have a mix of the following value types 
+//(you must use each one at least once)
+// Number, String, Array, Function, Object
+
+var house = {
+    doors: 3,
+    color: "orange",
+    rooms: 3,
+    garage: {
+        windows: 3,
+        doors: 1
+    }
+};
+
+var evenjumps = {
+    even_number: [2, 4, 6, 8, 10]
+}
+
+var funk = {
+    bass: 2,
+    funky: function() {
+        alert("it's running")
+    }
+};
--- readme.md
+++ readme.md
@@ -1,3 +1,22 @@
+
+June 11
+Also, create another JS file in your JS homework repo from last night and I want you do do the following in it.
+
+Preform some simple operations with numbers
+Preform some simple conditional statements using "if" and "else" and make sure to use variables
+Create 10 different objects whose properties have a mix of the following value types (you must use each one at least once)
+                -  Number
+                -  String
+                -  Array
+                -  Function
+                -  Object
+
+Make sure for all of your JS homework you are commenting what you are doing. I should be able to read it and learn about what you were thinking and why. Please have some fun with this, don't create arbitrary words but create something meaningful... like a person object or an animal object etc.
+
+
+
+
+June 10th
 Homework for tonight:
 
 1. Make sure all of your current homework is complete and that you have a STRONG understanding of the work we have done so far. If not please touch base with Shane or myself. 
